0e42b15bd339e91ba8dc93145f0392129447c879
kim <s158614@nishitech.ac.jp>
2019-12-16 10:52:00 +0900

    first react coponent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import Coffee from "./Coffee";
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        첫 리엑트 페이지
-      </h1>
+    <div>
+        <h1>hello</h1>
+        <Coffee />
     </div>
   );
 }
---
+++ src/Coffee.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Coffee() {
+
+    return <h3>I love coffee</h3>;
+
+}
+
+export default Coffee;
